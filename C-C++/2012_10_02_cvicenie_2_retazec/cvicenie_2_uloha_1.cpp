6ad1ec17d995c287ac33bcb2afb8d1cfc91219fb
Matej Bellus <matej.bellus@gmail.com>
2012-10-14 15:43:12 +0200

    male zmeny

---
+++ C-C++/2012_10_02_cvicenie_2_retazec/cvicenie_2_uloha_1.cpp
@@ -0,0 +1,14 @@
+/*
+ * cvicenie_2_uloha_1.cpp 	
+ * Vypisat retazec (string)
+ * Autor: 			Matej Bellus
+ * Date: 			2.10. 2012
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\t\\*/\"Toto\" je na '100%%' zarucene\\*/\n");
+	return 0;
+}
